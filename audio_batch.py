8c265cc31cf2105a5469deafb6c4309f658af5af
Aaron Boyd <aaronyo@Tupelo.local>
2009-12-26 13:06:33 -0800

    diffing now works

--- audio_batch.py
+++ audio_batch.py
@@ -19,7 +19,7 @@ def _determineConfigFileAbs():
 def _parseConfig( confFileAbs ):
 
     def _parseJob( confParser, jobName ):
-        sourceFilePatterns = confParser.get( jobName, "source_file_patterns" ).split( ';' )
+        sourceFilePatterns = confParser.get( jobName, "source_file_extensions" ).split( ';' )
         jobConfig = _Config.JobConfig( confParser.get( jobName, "source_path" ),
                                        sourceFilePatterns,
                                        confParser.get( jobName, "target_path" ),
@@ -35,18 +35,44 @@ def _parseConfig( confFileAbs ):
 
     return config
 
-def _all_file_paths(baseDirAbs, patterns):
+def _all_file_paths(baseDirAbs, extensions=None):
+    paths = set()
     for path, subdirs, files in os.walk(baseDirAbs):
         for name in files:
+            patterns = []
+            if extensions == None:
+                patterns.append('*')
+            else:
+                for ext in extensions:
+                    patterns.append('*' + os.extsep + ext)
             for pattern in patterns:
                 if fnmatch.fnmatch(name, pattern):
                     absolutePath = os.path.join(path, name)
-                    yield os.path.relpath( absolutePath, baseDirAbs )
+                    paths.add( os.path.relpath( absolutePath, baseDirAbs ) )
                     break
 
+    return paths
 
-def _diff_paths( source, target, ignoreExtensions=True ):
-    pass
+def _strip_extensions( filePaths, extensions ):
+    stripped = set()
+    for path in filePaths:
+        for ext in extensions:
+            fileSuffix = os.extsep + ext
+            if path.endswith( fileSuffix ):
+                stripped.add( path[: -len(fileSuffix) ] )
+    return stripped
+
+
+def _diff_paths( sources, targets, sourceExtensions, targetExtension, ignoreExtensions=True ):
+    if ignoreExtensions:
+        if sourceExtensions == None or targetExtension == None:
+            raise Exception("Must supply extensions to ignore")
+
+        sources = _strip_extensions( sources, sourceExtensions )
+        targets = _strip_extensions( targets, [targetExtension] )
+    
+    return sources.difference( targets )
+
 
 def _convert( sourceDir, targetDir, paths, audioFormat ):
     pass
@@ -60,15 +86,15 @@ class _Config:
         for jobName, jobConfig in self.jobConfigs.items():
             strRep += "  %s:\n" % jobName
             strRep += "    source: %s\n" % jobConfig.sourcePathAbs
-            strRep += "    source patterns: %s\n" % jobConfig.sourceFilePatterns
+            strRep += "    source patterns: %s\n" % jobConfig.sourceFileExtensions
             strRep += "    target: %s\n" % jobConfig.targetPathAbs
             strRep += "    shell command: %s\n" % jobConfig.shellCmd
         return strRep
         
     class JobConfig:        
-        def __init__( self, sourcePathAbs, sourceFilePatterns, targetPathAbs, shellCmd ):
+        def __init__( self, sourcePathAbs, sourceFileExtensions, targetPathAbs, shellCmd ):
             self.sourcePathAbs = sourcePathAbs
-            self.sourceFilePatterns = sourceFilePatterns
+            self.sourceFileExtensions = sourceFileExtensions
             self.targetPathAbs = targetPathAbs
             self.shellCmd = shellCmd
 
@@ -79,15 +105,21 @@ def main(args):
     print( config )
     jobConfig = config.jobConfigs['original_to_portable']
     i = 0
-    for sourceFile in _all_file_paths( jobConfig.sourcePathAbs, jobConfig.sourceFilePatterns ):
+
+    sourcePaths = _all_file_paths( jobConfig.sourcePathAbs, jobConfig.sourceFileExtensions )
+    for sourceFile in sourcePaths:
         i += 1
         if i == 100:
             print sourceFile
     print i
-    for sourceFile in _all_file_paths( jobConfig.targetPathAbs, jobConfig.sourceFilePatterns ):
+
+    targetPaths = _all_file_paths( jobConfig.targetPathAbs )
+    for sourceFile in targetPaths:
         i += 1
     print i
 
+    diff = _diff_paths( sourcePaths, targetPaths, jobConfig.sourceFileExtensions, 'mp3' )
+    print len(diff)
     
 
 if __name__ == "__main__":
