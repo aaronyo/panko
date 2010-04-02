f4772810578eac28a365f76c434cf8bf632d4ce5
Aaron Boyd <aaronyo@Tupelo.local>
2010-04-01 23:37:46 -0700

    added test for saving a tag

--- test/test_audiobatch/__init__.py
+++ test/test_audiobatch/__init__.py
@@ -4,8 +4,13 @@ import os.path
 from audiobatch.model import format
 
 test_data_dir = os.path.join( os.environ["AUDIOBATCH_HOME"], "test", "data" )
+temp_data_dir = os.sep + os.path.join( "tmp", "audiobatch" )
+
+if not os.path.exists ( temp_data_dir ):
+    os.makedirs( temp_data_dir )
 
 def notags_path( container_format ):
     filename = ( "notags" + os.extsep +
                  format.container_to_ext(container_format) )
     return os.path.join( test_data_dir, filename )
+
--- test/test_audiobatch/test_audiofile.py
+++ test/test_audiobatch/test_audiofile.py
@@ -1,25 +1,42 @@
+import os.path
+import shutil
 import unittest
 
 from audiobatch.persistence import audiofile
-from audiobatch.model import format
+from audiobatch.model import format, track
 import test_audiobatch
 
 
 class TestAudioFileTags( unittest.TestCase ):
 
+    @classmethod
+    def _tempCopy( cls, path ):
+        basename = os.path.basename( path )
+        copy_path = os.path.join( test_audiobatch.temp_data_dir,
+                                  cls.__name__ + '_' + basename )
+        shutil.copyfile( path, copy_path )
+        return copy_path
+
     def setUp( self ):
-        pass
+        path = test_audiobatch.notags_path( self._container_type )
+        copy_path = TestAudioFileTags._tempCopy( path )
+        self._audio_file = audiofile.read( copy_path )
 
     def tearDown( self ):
-        pass
+        os.unlink( self._audio_file.path )
 
     def test_open__no_tags( self ):
-        path = test_audiobatch.notags_path( self._container_type )
-        audio_file = audiofile.read( path )
-        track_info = audio_file.get_track_info()
-        print track_info.tags()
+        track_info = self._audio_file.get_track_info()
         self.assertTrue( track_info.is_empty() )
 
+    def test_save__title( self ):
+        track_info = track.TrackInfo()
+        track_info.title = "foo title"
+        self._audio_file.extend_track_info( track_info )
+        self._audio_file.save()
+        reloaded_af = audiofile.read( self._audio_file.path )
+        reloaded_ti = reloaded_af.get_track_info()
+        self.assertEqual( track_info.title, reloaded_ti.title )
 
 class TestMP4Tags( TestAudioFileTags ):
 
@@ -28,5 +45,23 @@ class TestMP4Tags( TestAudioFileTags ):
         TestAudioFileTags.setUp( self )
 
 
+class TestMP3Tags( TestAudioFileTags ):
+
+    def setUp( self ):
+        self._container_type = format.MP3_CONTAINER
+        TestAudioFileTags.setUp( self )
+
+
+class TestFLACTags( TestAudioFileTags ):
+
+    def setUp( self ):
+        self._container_type = format.FLAC_CONTAINER
+        TestAudioFileTags.setUp( self )
+
 def suite():
-    return unittest.TestLoader().loadTestsFromTestCase( TestMP4Tags )
+    suite = unittest.TestLoader().loadTestsFromTestCase( TestMP4Tags )
+    suite.addTests(
+        unittest.TestLoader().loadTestsFromTestCase( TestMP3Tags ) )
+    suite.addTests(
+        unittest.TestLoader().loadTestsFromTestCase( TestFLACTags ) )
+    return suite
