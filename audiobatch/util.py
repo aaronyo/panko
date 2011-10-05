59753c49ea78ea8db0d64c1da42790b7f91b1810
Aaron Boyd <aaronyo@yoboyd.com>
2011-10-04 23:54:44 -0700

    FIXME added

--- audiobatch/util.py
+++ audiobatch/util.py
@@ -3,7 +3,8 @@ def seqify(value):
         return value
     else:
         return [value]
-        
+
+#FIXME: Should support escaping the delim character
 def join_items(seq, delim):
     if hasattr(seq, '__iter__') and all(isinstance(val, str) or isinstance(val, unicode) for val in seq):
         return [delim.join(seq)]
