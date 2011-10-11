5e9b6da8c9dd8b4f1bc75afd5bc8635eb5cf0705
Aaron Boyd <aaronyo@yoboyd.com>
2011-10-11 02:32:01 -0700

    add dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import imp
 setup(
     name='panko',
     packages=find_packages(),
+    install_requires = ['mutagen', 'PIL'],
     entry_points = { 'console_scripts':['panko = panko.command.pankocmd:main'] },
     package_data = { 'panko.audiofile': ['tagmap.yaml'] }
 )
