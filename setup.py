675c9a711c6b85dfb69c0190de388bc9e29ade4c
Aaron Boyd <aaronyo@yoboyd.com>
2011-10-12 09:47:49 -0700

    add pyyaml to dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import imp
 setup(
     name='panko',
     packages=find_packages(),
-    install_requires = ['mutagen', 'PIL'],
+    install_requires = ['mutagen', 'PIL', 'pyyaml'],
     entry_points = { 'console_scripts':['panko = panko.command.pankocmd:main'] },
     package_data = { 'panko.audiofile': ['tagmap.yaml'] }
 )
