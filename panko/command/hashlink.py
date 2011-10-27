e13c269fa7c61ecda22784f683a934547306bb90
Aaron Boyd <aaronyo@yahoo.com>
2011-10-26 17:52:13 -0700

    tolerate fault on a particular file

--- panko/command/hashlink.py
+++ panko/command/hashlink.py
@@ -19,34 +19,37 @@ def main():
     hashlinks = os.listdir(args.out_dir)
             
     for filename in args.files:
-        md5val = audiofile.open(filename).audio_md5()
-        _, ext = os.path.splitext(filename)
-        matches = [os.path.join(args.out_dir, hl) for hl in hashlinks if fnmatch.fnmatch(hl, "%s*" % md5val)]
-        if matches:
-            already_linked = False
-            for match in matches:
-                 inode = os.stat(os.readlink(match)).st_ino
-                 source_inode = os.stat(filename).st_ino
-                 if inode == source_inode:
-                     print "%s: File already linked: %s" % (filename, os.path.basename(match))
-                     already_linked = True
+        try:
+            md5val = audiofile.open(filename).audio_md5()
+            _, ext = os.path.splitext(filename)
+            matches = [os.path.join(args.out_dir, hl) for hl in hashlinks if fnmatch.fnmatch(hl, "%s*" % md5val)]
+            if matches:
+                already_linked = False
+                for match in matches:
+                     inode = os.stat(os.readlink(match)).st_ino
+                     source_inode = os.stat(filename).st_ino
+                     if inode == source_inode:
+                         print "%s: File already linked: %s" % (filename, os.path.basename(match))
+                         already_linked = True
                      
-            if already_linked:
-                continue
+                if already_linked:
+                    continue
                 
-            matches = sorted([os.path.splitext(m)[0] for m in matches])
-            last_dupe_match = re.match('.+dupe-(\d+)', matches[-1])
-            if last_dupe_match:
-                dupe_num = int(last_dupe_match.group(1)) + 1
+                matches = sorted([os.path.splitext(m)[0] for m in matches])
+                last_dupe_match = re.match('.+dupe-(\d+)', matches[-1])
+                if last_dupe_match:
+                    dupe_num = int(last_dupe_match.group(1)) + 1
+                else:
+                    dupe_num = 1
+                dupe_part = "-dupe-%02i" % dupe_num
             else:
-                dupe_num = 1
-            dupe_part = "-dupe-%02i" % dupe_num
-        else:
-            dupe_part = ""
-        fname = md5val + dupe_part + ext
-        link_target = os.path.join(args.out_dir, fname)
-        os.symlink(os.path.abspath(filename), link_target)
-        hashlinks.append(fname)
+                dupe_part = ""
+            fname = md5val + dupe_part + ext
+            link_target = os.path.join(args.out_dir, fname)
+            os.symlink(os.path.abspath(filename), link_target)
+            hashlinks.append(fname)
+        except Exception as e:
+            print 'Trouble with file %s: %s' % (filename, e)
         
         
 if __name__ == '__main__':
