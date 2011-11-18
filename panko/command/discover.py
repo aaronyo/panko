3b53e727fef65b95eda59c47d6b3d4b155f7ae88
Aaron Boyd <aaronyo@yahoo.com>
2011-11-17 16:13:51 -0800

    extend echonest discovery

--- panko/command/discover.py
+++ panko/command/discover.py
@@ -15,6 +15,8 @@ def parse_args():
                         default=False, action='store_true')
     parser.add_argument('-e', '--echonest-id', help='discover echonest id for this file',
                         default=False, action='store_true')
+    parser.add_argument('-p', '--echonest-profile', help='discover echonest profile for this file',
+                        default=False, action='store_true')
     parser.add_argument('-i', '--if-needed',
                         help='only discover if no value is already present in the file',
                         default=False, action='store_true')
@@ -23,12 +25,14 @@ def parse_args():
                         default=False, action='store_true')
     parser.add_argument('-f', '--cover-format', type=str, help='file or url for cover art',
                         default=None)
+    parser.add_argument('--codegen-start', type=int, default=None)
+    parser.add_argument('--codegen-duration', type=int, default=None)
     return parser.parse_args()
     
 def main():
     args = parse_args()
     for filepath in args.files:
-        try:
+#        try:
             target_file = audiofile.open(filepath)
             tags = target_file.read_tags()
             if args.cover \
@@ -43,21 +47,53 @@ def main():
                     print cover_url
             elif args.echonest_id \
             and (not args.if_needed or 'echonest_id' not in target_file.read_tags()):
-                echonest.config_service()
+                echonest.config_service(codegen_start_=args.codegen_start,
+                                        codegen_duration_=args.codegen_duration)
                 echonest_id = echonest.song.identify(filepath,
                                                      codegen_start = echonest.codegen_start,
                                                      codegen_duration = echonest.codegen_duration)[0].id
-
                 if args.update:
                     target_file.write_tags({'echonest_id':echonest_id})
                     print '%s: updated with id %s' % (filepath, echonest_id)
                 else:
                     print echonest_id
+            elif args.echonest_profile:
+                echonest.config_service()
+                buckets=["audio_summary", "artist_familiarity", "artist_hotttnesss",
+                         "artist_location", "song_hotttnesss", "tracks", "id:7digital-US"]
+                song = echonest.song.profile( target_file.read_tags()['echonest_id'],
+                                              buckets=buckets )[0]
+                track = echonest.track.track_from_id(target_file.read_tags()['echonest_id'])
+                if args.update:
+                    target_file.write_tags({'echonest_id':echonest_id})
+                    print '%s: updated with id %s' % (filepath, echonest_id)
+                else:
+                    print "Audio Summary"
+                    print song.audio_summary
+                    print
+                    print "Artist Familiarity"
+                    print song.artist_familiarity
+                    print
+                    print "Artist Hotttnesss"
+                    print song.artist_hotttnesss
+                    print
+                    print "Artist Location"
+                    print song.artist_location
+                    print
+                    print "Song Hotttnesss"
+                    print song.song_hotttnesss,
+                    print
+                    print "Tracks"
+                    song = echonest.song.Song(target_file.read_tags()['echonest_id'])
+                    print song.get_tracks('7digital')[0]
+                    print
+                    print "Track analysis"
+                    print track.__dict__
             else:
                 print '%s: nothing to do' % filepath
-        except Exception as e:
-            print 'Trouble with file %s: %s' % (filepath, e)
-        sys.stdout.flush()
+#        except Exception as e:
+#            print 'Trouble with file %s: %s' % (filepath, e)
+            sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
--- panko/service/echonest.py
+++ panko/service/echonest.py
@@ -10,15 +10,16 @@ codegen_duration = None
 def config_service( api_key=None,
                     base_url=None,
                     codegen_cmd=None,
-                    sample_start=None,
-                    sample_duration=None):
+                    # FIXME: lame var names
+                    codegen_start_=None,
+                    codegen_duration_=None):
     config = ConfigParser.ConfigParser()
     config.read( pkg_resources.resource_filename(__name__, 'echonest.ini') )
     pyechonest.config.ECHO_NEST_API_KEY = api_key or config.get('web_service', 'api_key')
     base_url = base_url or config.get('web_service', 'base_url')
     pyechonest.config.CODEGEN_BINARY_OVERRIDE = codegen_cmd or config.get('codegen', 'shell_command')
     global codegen_start, codegen_duration
-    codegen_start = sample_start \
+    codegen_start = codegen_start \
                     or int( config.get( 'codegen', 'sample_start') )
-    codegen_duration = sample_duration \
+    codegen_duration = codegen_duration \
                        or int( config.get( 'codegen', 'sample_duration') )
